db00b0f73f5a7c3909929b647a8bd0d74e252806
Chris Lange <clange@developingit.com>
2019-08-08 15:44:48 +0100

    fix backup copy source and delete

--- files/backup.sh
+++ files/backup.sh
@@ -8,8 +8,9 @@ sudo cya keep name ${HOSTNAME}_${TODAY} archive
 
 # Copy backup to remote
 if [ -d "/mnt/backup/$HOSTNAME/cya" ]; then
-	sudo cp -r /home/cya/* /mnt/backup/$HOSTNAME/cya/
+	sudo cp -r /home/cya/archives/* /mnt/backup/$HOSTNAME/cya/
 else
     sudo mkdir /mnt/backup/$HOSTNAME/cya
-    sudo cp -r /home/cya/* /mnt/backup/$HOSTNAME/cya/
+    sudo cp -r /home/cya/archives/* /mnt/backup/$HOSTNAME/cya/
 fi
+sudo rm -r /home/cya/archives/*
