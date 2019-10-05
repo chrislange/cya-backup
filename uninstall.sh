1b8b13f3d4d9ee624d0680618b5e83fc7d33b2e6
Chris <chrislange3@gmail.com>
2019-10-05 22:18:05 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,10 +5,7 @@ sudo rm -r /mnt/backup
 sudo rm -r /usr/local/bin/cya
 sudo rm -r /home/cya
 sudo rm -r /scripts/cya-backup
-sudo rm /etc/cron.hourly/cya-backup.sh
-sudo rm /etc/cron.daily/cya-backup.sh
-sudo rm /etc/cron.weekly/cya-backup.sh
-sudo rm /etc/cron.monthly/cya-backup.sh
+sudo rm /etc/cron*/cya-backup.sh
 clear
 
 echo
